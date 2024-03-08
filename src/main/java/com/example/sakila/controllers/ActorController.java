920f6c51850deaf115c20537f19375c5a43a045b
Axel <agh2172@columbia.edu>
2024-03-08 09:51:46 +0000

    Removed old commented out code

--- src/main/java/com/example/sakila/controllers/ActorController.java
+++ src/main/java/com/example/sakila/controllers/ActorController.java
@@ -55,26 +55,6 @@ public class ActorController {
 
     @PatchMapping("/actors")
     public Actor updateActor(@Validated @RequestBody ActorUpdate data){
-//        short id = data.getId();
-//        final var actor = actorRespository.findById(id).orElseThrow(() -> new ResourceAccessException("No Such Actor"));
-//
-//        String firstName = data.getFirstName();
-//        String lastName = data.getLastName();
-//
-//        if(firstName != null)  actor.setFirstName(firstName);
-//        if(lastName != null) actor.setLastName(lastName);
-//        List<Short> films = data.getFilms();
-//        if(!films.isEmpty()) {
-//            List<PartialFilm> currentFilms = new ArrayList<PartialFilm>();
-//            for (Short filmId : films) {
-//                //get film to add
-//                final var film = filmRepository.findById(filmId).orElseThrow(() -> new ResourceAccessException("No Such Film"));
-//                //add film
-//                PartialFilm partialFilm = film.toPartialFilm();
-//                currentFilms.add(partialFilm);
-//            }
-//            actor.setFilms(currentFilms);
-//        }
         final var actor = actorService.updateActorService(data);
         return actorRespository.save(actor);
     }
@@ -82,14 +62,11 @@ public class ActorController {
     @DeleteMapping("/actors/{id}")
     @ResponseStatus(HttpStatus.ACCEPTED)
     public void deleteActor(@PathVariable Short id){
-        //final var actor = actorRespository.findById(id).orElseThrow(() -> new ResourceAccessException("No Such Actor"));
         actorRespository.deleteById(id);
     }
 
     @GetMapping("/starsIn/{id}")
     public List<PartialFilm> filmsStarredIn(@PathVariable Short id) {
-//        final var actor = actorRespository.findById(id).orElseThrow(() -> new ResourceAccessException("No Such Actor"));
-//        return actor.getFilms();
         return actorService.filmsStarredIn(id);
     }
 }
